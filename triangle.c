c48082ebe0b944f58f1f2a595dc24c04976d04a5
vitalkuz <vitalups@mail.ru>
2016-09-16 22:58:19 +0300

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -22,16 +22,16 @@ float x1, y1, x2, y2, x3, y3, xa, ya;
   scanf("%f", &ya);
 
   if ((((x3 - x1) / (x2 - x1)) - ((y3 - y1) / (y2 - y1))) * (((xa - x1) / (x2 - x1)) - ((ya - y1) / (y2 - y1))) <= 0) {
-  printf("\n no \n");
-  return 0;
+    printf("\n no \n");
+    return 0;
   }
   if ((((x2 - x3) / (x1 - x3)) - ((y2 - y3) / (y1 - y3))) * (((xa - x3) / (x1 - x3)) - ((ya - y3) / (y1 - y3))) <= 0) {
-  printf("\n no \n");
-  return 0;
+    printf("\n no \n");
+    return 0;
   }
   if ((((x1 - x2) / (x3 - x2)) - ((y1 - y2) / (y3 - y2))) * (((xa - x2) / (x3 - x2)) - ((ya - y2) / (y3 - y2))) <= 0) {
-  printf("\n no \n");
-  return 0;
+    printf("\n no \n");
+    return 0;
   }
   printf("\n yes \n");
 return 0;

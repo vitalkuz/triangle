98235508d8c29e95d229d6601429312b7729db50
vitalkuz <vitalups@mail.ru>
2016-09-24 01:46:50 +0400

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 
+int check_two_dots(float x_testable, float y_testable, float x1_line_dot, float y1_line_dot, float x2_line_dot, float y2_line_dot, float x_checked_dot, float y_checked_dot) {
+  
+  if ((((x_testable - x1_line_dot) / (x2_line_dot - x1_line_dot)) - ((y_testable - y1_line_dot) / (y2_line_dot - y1_line_dot))) * (((x_checked_dot - x1_line_dot) / (x2_line_dot - x1_line_dot)) - ((y_checked_dot - y1_line_dot) / (y2_line_dot - y1_line_dot))) <= 0) {
+	  printf("\n no \n");
+	  return 1;
+	}
+	return 0;
+}
 int main() {
 
-float x1, y1, x2, y2, x3, y3, xa, ya;
+	float x1, y1, x2, y2, x3, y3, xa, ya;
 
-  printf("Enter triangle coordinates\n1 dot\nx= ");
-  scanf("%f", &x1);
-  printf("\ny= ");
-  scanf("%f", &y1);
-  printf("\n2 dot\nx= ");
-  scanf("%f", &x2);
-  printf("\ny= ");
-  scanf("%f", &y2);
-  printf("\n3 dot\nx= ");
-  scanf("%f", &x3);
-  printf("\ny= ");
-  scanf("%f", &y3);
-  printf("\n Enter dot coordinates:\nx= ");
-  scanf("%f", &xa);
-  printf("\ny= ");
-  scanf("%f", &ya);
+	printf("Enter triangle coordinates\n1 dot\nx= ");
+	scanf("%f", &x1);
+	printf("\ny= ");
+	scanf("%f", &y1);
+	printf("\n2 dot\nx= ");
+	scanf("%f", &x2);
+	printf("\ny= ");
+	scanf("%f", &y2);
+	printf("\n3 dot\nx= ");
+	scanf("%f", &x3);
+	printf("\ny= ");
+	scanf("%f", &y3);
+	printf("\n Enter dot coordinates:\nx= ");
+	scanf("%f", &xa);
+	printf("\ny= ");
+	scanf("%f", &ya);
 
-  if ((((x3 - x1) / (x2 - x1)) - ((y3 - y1) / (y2 - y1))) * (((xa - x1) / (x2 - x1)) - ((ya - y1) / (y2 - y1))) <= 0) {
-    printf("\n no \n");
-    return 0;
-  }
-  if ((((x2 - x3) / (x1 - x3)) - ((y2 - y3) / (y1 - y3))) * (((xa - x3) / (x1 - x3)) - ((ya - y3) / (y1 - y3))) <= 0) {
-    printf("\n no \n");
-    return 0;
-  }
-  if ((((x1 - x2) / (x3 - x2)) - ((y1 - y2) / (y3 - y2))) * (((xa - x2) / (x3 - x2)) - ((ya - y2) / (y3 - y2))) <= 0) {
-    printf("\n no \n");
-    return 0;
-  }
-  printf("\n yes \n");
-return 0;
+	if (check_two_dots(x3, y3, x1, y1, x2, y2, xa, ya) == 1) {
+	  return 0;
+	}
+	if (check_two_dots(x2, y2, x3, y3, x1, y1, xa, ya) == 1) {
+	  return 0;
+	}
+	if (check_two_dots(x1, y1, x2, y2, x3, y3, xa, ya) == 1) {
+	  return 0;
+	}
+	
+	printf("\n yes \n");
+	
+	return 0;
 }
